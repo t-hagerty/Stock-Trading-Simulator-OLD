5327a39a757aead53f95facffaae9f211c61cbbe
Tyler Hagerty <thagerty_0213@email.ric.edu>
2019-08-13 23:43:13 -0400

    Basic Company Modeling -Added a basic model class for storing Company information. -Added a basic/temp Controller and View for displaying a list of Companies.

---
+++ StockSimulator/Controllers/CompanyController.cs
@@ -0,0 +1,134 @@
+﻿using StockSimulator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StockSimulator.Controllers
+{
+    public class CompanyController : Controller
+    {
+        // GET: Company
+        public ActionResult Index()
+        {
+            var companies = from c in GetCompanyList()
+                            orderby c.ID
+                            select c;
+            return View(companies);
+        }
+
+        // GET: Company/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Company/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Company/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Company/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Company/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Company/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Company/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [NonAction]
+        public List<Company> GetCompanyList()
+        {
+            return new List<Company>{
+                new Company{
+                    ID = 1,
+                    CompanyName = "Alphabet Inc",
+                    TickerSymbol = "GOOG",
+                    StartDataDate = DateTime.Parse(DateTime.Today.ToString())
+                },
+
+                new Company{
+                    ID = 2,
+                    CompanyName = "Apple Computer Inc",
+                    TickerSymbol = "AAPL",
+                    StartDataDate = DateTime.Parse(DateTime.Today.ToString())
+                },
+
+                new Company{
+                    ID = 3,
+                    CompanyName = "Facebook Inc",
+                    TickerSymbol = "FB",
+                    StartDataDate = DateTime.Parse(DateTime.Today.ToString())
+                },
+
+                new Company{
+                    ID = 4,
+                    CompanyName = "Intel Corporation",
+                    TickerSymbol = "INTC",
+                    StartDataDate = DateTime.Parse(DateTime.Today.ToString())
+                },
+
+                new Company{
+                    ID = 5,
+                    CompanyName = "Microsoft Corporation",
+                    TickerSymbol = "MSFT",
+                    StartDataDate = DateTime.Parse(DateTime.Today.ToString())
+                },
+            };
+        }
+    }
+}
---
+++ StockSimulator/Models/Company.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StockSimulator.Models
+{
+    public class Company
+    {
+        public int ID { get; set; }
+        public string CompanyName { get; set; }
+        public string TickerSymbol { get; set; }
+        public DateTime StartDataDate { get; set; }
+    }
+}
